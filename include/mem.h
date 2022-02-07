51102f05b049fdcac87b267af49866d72d3d6093
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2022-02-08 00:44:31 +0400

    👷 add guard

--- include/mem.h
+++ include/mem.h
@@ -31,6 +31,8 @@
 *
 ****************************************************************************/
 
+#ifndef __X86EMU_MEM_H
+#define __X86EMU_MEM_H
 
 unsigned vm_memio(x86emu_t *emu, u32 addr, u32 *val, unsigned type);
 x86emu_mem_t *emu_mem_new(unsigned perm);
@@ -38,3 +40,4 @@ x86emu_mem_t *emu_mem_free(x86emu_mem_t *mem);
 x86emu_mem_t *emu_mem_clone(x86emu_mem_t *mem);
 void *mem_dup(const void *src, size_t n);
 
+#endif /* __X86EMU_MEM_H */
